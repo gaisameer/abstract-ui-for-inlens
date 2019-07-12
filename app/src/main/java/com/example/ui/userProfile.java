b8080068563eb9dd821cb396df42a7c75dd7a55d
gaisameer <gaisameer@cet.ac.in>
2019-07-12 21:45:51 +0530

    user profile

--- app/src/main/java/com/example/ui/userProfile.java
+++ app/src/main/java/com/example/ui/userProfile.java
@@ -3,6 +3,8 @@ package com.example.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class userProfile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class userProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void clicked(View view) {
+
+        Toast.makeText(this,"clicked",Toast.LENGTH_SHORT).show();
     }
 }
